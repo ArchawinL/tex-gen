88255dd90ecb131b15a99f62b4dd0013b4e2f887
ArchawinL <a.leelapanang@gmail.com>
2025-09-05 18:50:59 +0700

    Basic backend works

--- backend/model.py
+++ backend/model.py
@@ -1,5 +1,14 @@
+import os 
+from pathlib import Path
+from dotenv import load_dotenv
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 import torch
+
+
+dotenv_path = Path(__file__).parent / ".env"
+load_dotenv(dotenv_path)
+HF_TOKEN = os.getenv("HUGGINGFACE_HUB_TOKEN")
+print(HF_TOKEN[:5])
 
 
 MODEL_NAME = "mistralai/Mistral-7B-Instruct-v0.2"
@@ -7,13 +16,13 @@ MODEL_NAME = "mistralai/Mistral-7B-Instruct-v0.2"
 print(f"Loading {MODEL_NAME}")
 
 
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
-
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, token=HF_TOKEN)
 model = AutoModelForCausalLM.from_pretrained(
     MODEL_NAME,
-    torch_dtype=torch.float16 if torch.cuda.is_available() else torch.float32,
-    device_map="auto"
+    dtype=torch.float16 if torch.cuda.is_available() else torch.float32,
+    device_map="auto",
+    token=HF_TOKEN
 )
 
 
@@ -21,6 +30,5 @@ generator = pipeline(
     "text-generation",
     model=model,
     tokenizer=tokenizer,
-    device=0 if torch.cuda.is_available() else -1
 )
 
