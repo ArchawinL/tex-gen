a8cd7b022a938ba25d767a5585cfdaf57db7eab7
ArchawinL <a.leelapanang@gmail.com>
2025-09-05 12:32:26 +0700

    Client calls

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,40 @@
+import uvicorn
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from .model import generator
 
 
+app = FastAPI()
 
 
-def main():
-    pass
+@app.post("/chat/")
+async def chat(message: dict):
+    user_input = message["text"]
+    outputs = generator(
+        user_input,
+        max_new_tokens=200,
+        do_sample=True,
+        temperature=0.7
+    )
+    
+    return {"response": outputs[0]["generated_text"]}
 
 
+@app.websocket("/ws")
+async def websocket_endpoint(ws: WebSocket):
+    await ws.accept()
 
+    try:
+        while True:
+            data = await ws.receive_text()
+            outputs = generator(
+                data,
+                max_new_tokens=200,
+                do_sample=True,
+                temperature=0.7
+            )
+            reply = outputs[0]["generated_text"]
+            await ws.send_text(reply)
+    
+    except WebSocketDisconnect:
+        print("Client Disconnected")
 
-if __name__  == '__main__':
-    main()
