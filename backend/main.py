597b73e06138ad4d38ba07eced94a5e74ae057f0
ArchawinL <a.leelapanang@gmail.com>
2025-09-19 11:31:25 +0100

    imports fixed and chat

--- backend/main.py
+++ backend/main.py
@@ -1,24 +1,32 @@
 import uvicorn
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from .model import generator
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
+from model import generator, generate_response
 
+# Always activate venv first
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173","http://127.0.0.1:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class ChatRequest(BaseModel):
+    text: str
+
+
 @app.get("/")
 async def root():
     return {"message": "FastAPI backend is running"}
 
 @app.post("/chat/")
-async def chat(message: dict):
-    user_input = message["text"]
-    outputs = generator(
-        user_input,
-        max_new_tokens=200,
-        do_sample=True,
-        temperature=0.7
-    )
-    
-    return {"response": outputs[0]["generated_text"]}
+async def chat(req: ChatRequest):
+    reply = generate_response(req.text)
+    return {"response": reply}
 
 
 @app.websocket("/ws")
